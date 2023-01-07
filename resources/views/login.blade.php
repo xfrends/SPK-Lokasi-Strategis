edeaf56f0617fc7a5182c9b3c3fd5643fe32c025
Frendi Triarista <frendi@thriveagency.id>
2023-01-07 14:00:05 +0700

    title

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>SPK Seleksi CPNS</title>
+    <title>SPK Lokasi Strategis</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
